8fafd0c646d6c3af2cc95d223fcbad5bb8f29513
Dragan Djuric <dragan@dragandjuric.com>
2010-04-22 22:59:14 +0200

    Some rogue tabs converted to spaces.

--- src/main/clojure/org/uncomplicate/magicpotion.clj
+++ src/main/clojure/org/uncomplicate/magicpotion.clj
@@ -4,18 +4,18 @@
   (:use [org.uncomplicate.magicpotion.validation :as validation])
   (:use org.uncomplicate.magicpotion.core)
   (:use [org.uncomplicate.magicpotion.m3 :as m3]))
-  
-(defn ref> 
+
+(defn ref>
   ([prop]
    (ref> prop nil))
   ([prop restrictions]
   {:pre [(property? prop)]}
-  (create-role-def (property-def prop) 
+  (create-role-def (property-def prop)
                    :by-reference
                    restrictions
-									 (::m3/hierarchy (meta prop)))))
+                   (::m3/hierarchy (meta prop)))))
 
-(defn ref*> 
+(defn ref*>
   ([prop]
    (ref*> prop nil))
   ([prop restrictions]
@@ -26,9 +26,9 @@
                         :by-reference
                         restrictions
                         set-restrictions
-												(::m3/hierarchy (meta prop)))))
+                        (::m3/hierarchy (meta prop)))))
 
-(defn val> 
+(defn val>
   ([prop]
    (val> prop nil))
   ([prop restrictions]
@@ -36,9 +36,9 @@
   (create-role-def (property-def prop)
                    :by-value
                    restrictions
-									 (::m3/hierarchy (meta prop)))))
+                   (::m3/hierarchy (meta prop)))))
 
-(defn val*> 
+(defn val*>
   ([prop ]
    (val*> prop nil))
   ([prop restrictions]
@@ -49,21 +49,21 @@
                         :by-value
                         restrictions
                         set-restrictions
-												(::m3/hierarchy (meta prop)))))
+                        (::m3/hierarchy (meta prop)))))
 
 (defmacro property
   [name & params]
-  {:pre [(every? (partial contains? #{:restrictions :super}) 
-								 (filter keyword? params))]}
+  {:pre [(every? (partial contains? #{:restrictions :super})
+                 (filter keyword? params))]}
   (let [pos (take-while (comp not keyword?) params)
         kw-map (apply hash-map (drop-while (comp not keyword?) params))
         restrictions (if-let [r (first pos)] r (:restrictions kw-map))
         super (if-let [s (second pos)] s (:super kw-map))]
-    `(let [property-def# (create-property-def 
-                     			 (to-keyword ~name)
+    `(let [property-def# (create-property-def
+                            (to-keyword ~name)
                            ~restrictions
                            (map property-def ~super))]
-     	(def ~name (create-property property-def#)))))
+       (def ~name (create-property property-def#)))))
 
 (defn sanitize-roles [raw-roles]
   (map #(if (property? %) (val> %) %) raw-roles))
@@ -71,19 +71,19 @@
 (defmacro concept
   ([name & params]
   {:pre [(every? (partial contains? #{:roles :restrictions :super})
-								 (filter keyword? params))]}
+                 (filter keyword? params))]}
   (let [pos (take-while (comp not keyword?) params)
         kw-map (apply hash-map (drop-while (comp not keyword?) params))
         roles (if-let [r (first pos)] r (:roles kw-map))
         super (if-let [s (second pos)] s (:super kw-map))
-				restrictions (set (if-let [re (second (rest pos))] re (:restrictions kw-map)))]
+        restrictions (set (if-let [re (second (rest pos))] re (:restrictions kw-map)))]
    `(let [name-keyword# (to-keyword ~name)
-          concept-def# (inherit-roles 
-											   (create-concept-def
-                         	 name-keyword#
-                         	 (sanitize-roles ~roles)
-													 ~restrictions
-                         	 (map concept-def ~super)))]
+          concept-def# (inherit-roles
+                         (create-concept-def
+                            name-keyword#
+                            (sanitize-roles ~roles)
+                           ~restrictions
+                            (map concept-def ~super)))]
       (do
         (def ~(suf-symbol name "?") (is-instance name-keyword#))
         (def ~name (create-concept concept-def#)))))))
